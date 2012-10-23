5e0905a8a90224797c6332908b7b7abaa1720182
Eric Chi <eric.c.chi@gmail.com>
2012-10-23 13:42:06 -0700

    Checking in changes to SONcluster.m

--- SONcluster.m
+++ SONcluster.m
@@ -14,7 +14,34 @@ V = zeros(p,p,n);
 X = zeros(n,p);
 S = zeros(n,p);
 
+%
+% Make some simulated data.
+%
+seed = 12345;
+stream = RandStream('mt19937ar','Seed',seed);
+RandStream.setGlobalStream(stream);
+X = 0.25*randn(n,p);
+X(:,1:25) = X(:,1:25) + ones(2,25);
+
+plot(X(1,:),X(2,:),'.')
+%%
 for i=1:p
     S(:,i) = X(:,i) - reshape(sum(L(i,:,:),2) - sum(L(:,i,:),1),n,1) ...
         + reshape(rho*(sum(V(i,:,:),2) - sum(V(:,i,:),1)),n,1); 
+end
+
+U = S*(1/nu)*(eye(p,p) - (2*rho/(1-4*rho*p))*ones(p,p));
+
+for i=2:p
+    for j=1:i-1
+        V(i,j,:) = U(:,i) - U(:,j) + (1/rho)*reshape(L(i,j,:),n,1);
+        V(j,i,:) = V(i,j,:);
+    end
+end
+
+for i=2:p
+    for j=1:i-1
+        L(i,j,:) = reshape(L(i,j,:),n,1) + rho*(U(:,i) - U(:,j) - reshape(V(i,j,:),n,1));
+        L(j,i,:) = L(i,j,:);
+    end
 end
